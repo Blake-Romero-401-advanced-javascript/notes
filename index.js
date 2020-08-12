c12d221365fa825b560893423a3ccf03b9dbdb0f
blakerom <blakeromero001@gmail.com>
2020-08-11 20:45:18 -0700

    finsihed refactor to classes

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 
 const Input = require('./lib/input.js');
 // const HTTP = require('./lib/http.js');
-// const Notes = require('./lib/notes.js');
+const Note = require('./lib/notes.js');
 
 const options = new Input();
 
+const command = new Note(options);
 // HTTP.fetch(options);
