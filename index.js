8a10b0ffb034bb3529fccab1eb66f6af47d9005a
blakerom <blakeromero001@gmail.com>
2020-08-11 21:54:04 -0700

    update lab 2

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const Input = require('./lib/input.js');
 // const HTTP = require('./lib/http.js');
 const Note = require('./lib/notes.js');
 
-const options = new Input();
+const input = new Input();
 
-const command = new Note(options);
+const command = new Note(input);
 // HTTP.fetch(options);
+
+input.valid() ? command.execute() : help();
+
+function help() {
+  console.log('Something went wrong!!!');
+  process.exit();
+}
